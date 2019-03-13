2354afadc93d894420c396562abb99e725a8e404
anidaniel <anidanielcorp@gmail.com>
2019-03-14 03:44:21 +0530

    main.js and node_modules addition

---
+++ main.js
@@ -0,0 +1,47 @@
+const SHA256 = require('crypto-js/sha256');
+
+
+class Block{
+	constructor(index, timestamp, data, previousHash = ''){
+		this.index = index;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.previousHash = previousHash;
+		this.hash = this.calculateHash();
+	}
+
+	calculateHash(){
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+
+	
+	}
+}
+
+class Blockchain{
+	constructor(){
+		this.chain = [this.createGenesisBlock()];
+	}
+
+	createGenesisBlock(){
+		return new Block(0, "01/01/2019", "Genesis Block", "0");
+	}
+
+	getLatestBlock(){
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(newBlock){
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	
+	}
+}
+
+let aniCoin = new Blockchain();
+
+aniCoin.addBlock(new Block(1, "05/02/2019", { amount: 4}));
+aniCoin.addBlock(new Block(2, "12/03/2019", { amount: 16}));
+
+
+console.log(JSON.stringify(aniCoin, null, 4));
